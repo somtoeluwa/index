2056e1746559622516adaff018f213e51c3276f4
somtoeluwa <1412632@rgu.ac.uk>
2016-03-07 14:11:00 +0000

    phplab testing

---
+++ CMM007-master/6-1-IntroToPhp/Lab 1 Solutions/Activity1-HelloWorld.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike Crabb
+ * Date: 27/10/15
+ * Time: 16:19
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+	</head>
+	<body>
+        <p>
+          <?php
+            echo "Hello World";
+          ?>
+        </p>
+	</body>
+</html>
