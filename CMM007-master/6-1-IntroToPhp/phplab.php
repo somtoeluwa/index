6bec4480927bce044f0972aa5e9d37676a129acb
somtoeluwa <1412632@rgu.ac.uk>
2016-03-07 09:49:35 +0000

    phplab testing

---
+++ CMM007-master/6-1-IntroToPhp/phplab.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1412632
+ * Date: 07/03/2016
+ * Time: 09:42
+ */
+
+echo "Hello, world";
+echo "Hello," . " ". "world". "!";
+
+echo 5*7;
+
+$myname = "Frodo baggins";
+$myage = 111;
+echo "My name is ". $myname . "and i am". $myage;
+
+
+?>
