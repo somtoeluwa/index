ec1145c11de6ea781fcf5314e420ee86a3f06631
somtoeluwa <1412632@rgu.ac.uk>
2016-03-14 11:00:24 +0000

    week5lab

---
+++ CMM007-master/week5/login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1412632
+ * Date: 14/03/2016
+ * Time: 10:49
+ */
+
+include("connection.php");
+
+
+    if(empty($_POST["username"]) || empty($_POST["password"]))
+    {
+        echo "Both Fields are required.";
+    }else
+    {
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+
+    }
