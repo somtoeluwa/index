14d332de9ec2591974e14f80c80a6b57bb0e9236
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 14:25:21 +0000

    Lab 7 part 2

---
+++ CMM007labs/marvelmovies/display_after2010.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 3/21/2016
+ * Time: 2:19 PM
+ */
+include "dbconnect.php";
+
+$sql =  "SELECT * FROM marvelmovies WHERE yearReleased > '2010'";
+
+$result=mysqli_query($db,$sql);
+while($row = $result->fetch_array())
+{
+    $movieTitle = $row['title'];
+    $yearReleased = $row['yearReleased'];
+    $productionStudio = $row['productionStudio'];
+    $notes = $row['notes'];
+
+    echo "<p>" . $movieTitle ."</p>";
+    echo "<p>" . $yearReleased ."</p>";
+    echo "<p>" . $productionStudio ."</p>";
+    echo "<p>" . $notes ."</p>";
+    echo "<br>";
+}
+
+?>
