6465442df295c4c7f25bb68ff86ca0aefb310a19
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 13:56:42 +0000

    Lab 7 part 2

--- CMM007labs/marvelmovies/display_all.php
+++ CMM007labs/marvelmovies/display_all.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -7,16 +10,16 @@
  */
 include "dbconnect.php";
 
-$sql =  "SELECT title FROM marvelmovies";
+$sql =  "SELECT * FROM marvelmovies WHERE productionStudio = 'Marvel Studios'";
 
 $result=mysqli_query($db,$sql);
 while($row = $result->fetch_array())
 {    /* the code inside here is repeated for each item in the array
     You can do things like the following to print out each movie title */
     $movieTitle = $row['title'];
+    $productionStudio = $row['productionStudio'];
 
-echo "<p>" . $movieTitle . "</p>";
-
+echo "<p>" . $movieTitle . $productionStudio. "</p>";
 }
 
 
