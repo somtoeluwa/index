9d3b291d9685b5f3e958584379ab8b8b7fafa02d
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 14:09:23 +0000

    Lab 7 part 2

--- CMM007labs/marvelmovies/display_all.php
+++ CMM007labs/marvelmovies/display_all.php
@@ -1,25 +1,26 @@
-
-
-
 <?php
 /**
  * Created by PhpStorm.
  * User: Sommy B
  * Date: 3/21/2016
- * Time: 1:34 PM
+ * Time: 2:04 PM
  */
 include "dbconnect.php";
 
-$sql =  "SELECT * FROM marvelmovies WHERE productionStudio = 'Marvel Studios'";
+$sql =  "SELECT * FROM marvelmovies";
 
 $result=mysqli_query($db,$sql);
 while($row = $result->fetch_array())
-{    /* the code inside here is repeated for each item in the array
-    You can do things like the following to print out each movie title */
+{
     $movieTitle = $row['title'];
+    $yearReleased = $row['yearReleased'];
     $productionStudio = $row['productionStudio'];
+    $notes = $row['notes'];
 
-echo "<p>" . $movieTitle ."   ". $productionStudio. "</p>";
+    echo "<p>" . $movieTitle ."</p>";
+    echo "<p>" . $yearReleased ."</p>";
+    echo "<p>" . $productionStudio ."</p>";
+    echo "<p>" . $notes ."</p>";
 }
 
 
---
+++ CMM007labs/marvelmovies/display_marvelstudios.php
@@ -0,0 +1,28 @@
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 3/21/2016
+ * Time: 1:34 PM
+ */
+include "dbconnect.php";
+
+$sql =  "SELECT * FROM marvelmovies WHERE productionStudio = 'Marvel Studios'";
+
+$result=mysqli_query($db,$sql);
+while($row = $result->fetch_array())
+{    /* the code inside here is repeated for each item in the array
+    You can do things like the following to print out each movie title */
+    $movieTitle = $row['title'];
+    $productionStudio = $row['productionStudio'];
+
+echo "<p>" . $movieTitle ."   ". $productionStudio. "</p>";
+}
+
+
+
+
+?>
