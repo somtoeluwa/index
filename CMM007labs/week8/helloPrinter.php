b8e7bd68698294cda91e1f63885c8ceac2dc52ba
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 18:15:24 +0000

    MOck labb corrections

---
+++ CMM007labs/week8/helloPrinter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 3/21/2016
+ * Time: 6:03 PM
+ */
+
+
+$query = $_GET["query"];
+echo "The query is ".$query ." ";
+
+
+?>
---
+++ CMM007labs/week8/helloSelector.php
@@ -0,0 +1,16 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+
+<body>
+<ul>
+<li><a href="helloPrinter.php?query=Earth"</a></li><br>
+<li><a href="helloPrinter.php?query=Mars"</a></li><br>
+<li><a href="helloPrinter.php?query=Jupiter"</a></li><br>
+
+</ul>
+</body>
+</html>
