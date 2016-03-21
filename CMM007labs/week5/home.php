ebd99b1e7eadf22e16ada8e5c44bc9d8c4ff2732
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-21 13:31:21 +0000

    Lab 7

--- CMM007labs/week5/home.php
+++ CMM007labs/week5/home.php
@@ -2,10 +2,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
-
 </head>
 
 <body>
+  <p>Curently logged in:<?php
+      include ("login.php");
+      echo $username;
+        ?> </p>
+
+
 <h1>Hello</h1>
 </body>
 </html>
