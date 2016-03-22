9ffddadbbda3817ce2aa7660c9d00a07828a9458
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-03-22 09:42:21 +0000

    lab Tuesday

---
+++ CMM007labs/week8/createhero.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SHIELD RECRUITER</title>
+</head>
+<body>
+<header>
+    <h2>New Super Hero Details</h2>
+</header>
+<main>
+    <form action="formResponse.php" method="post">
+        <label>Forename:</label><input type="text" name="forename"><br>
+        <label>Surname:</label><input type="text" name="surname">
+        <input type="submit" value="submit">
+    </form>
+
+</main>
+
+
+</body>
+</html>
---
+++ CMM007labs/week8/formResponse.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 3/22/2016
+ * Time: 9:35 AM
+ */
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+echo $forename . $surname;
+
+
+?>
