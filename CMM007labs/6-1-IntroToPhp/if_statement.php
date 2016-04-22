53596aba150dac923c6b8f210dfc0d4a76da5764
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-22 03:18:37 +0100

    IF statement from lab activity

---
+++ CMM007labs/6-1-IntroToPhp/if_statement.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 4/22/2016
+ * Time: 3:07 AM
+ */
+
+
+$myage;
+$specs = "Specs";
+$mugs = "mugs";
+$sausage = "Sausage rolls";
+
+
+if ($myage >= 21 ){
+
+    echo "<p> You can buy ".$sausage.", ". $mugs.", ". $specs."</p>";
+}
+elseif($myage >= 18 && $myage <21){
+
+    echo "<p> You can buy ".$specs.", ". $mugs."</p>";
+}
+elseif( $myage< 18 && $myage >= 16){
+
+    echo "<p> You can buy ".$specs."</p>";
+}
+else {
+    echo "<p> You cant buy anything here </p>";
+}
