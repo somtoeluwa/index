96464f292a36ae59b61c973f12d56ce5754734f4
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-22 04:19:55 +0100

    lqb 7

---
+++ CMM007labs/examprep/lab7/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+
+<form action="login.php" method="post">
+
+    Username:
+    <input type="text" name="username" value="">
+    <br>
+    Password:
+    <input type="text" name="password" value="">
+    <br>
+    <input type="submit" value="submit">
+
+
+</form>
+
+</body>
+</html>
