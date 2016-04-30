9ef9b0919d8615666e124337971354a4616faa42
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-04-30 21:33:27 +0100

    exam prep

---
+++ CMM007labs/examprep/week10/printPOST.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<article>
+    <h1><?php $_GET["text"]?></h1>
+    <p><?php $_GET["content"]?></p>
+    <strong><?php $_GET["cat"]?></strong>
+</article>
+
+
+</body>
+</html>
